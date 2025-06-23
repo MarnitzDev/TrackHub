8a1e7fa1c84782ba8f4999a841bdf077d4527e51
MarnitzDev <malanmarnitz@gmail.com>
2025-06-23 12:06:15 +0200

    cardModal, cardEditor

--- components/Card/Item.vue
+++ components/Card/Item.vue
@@ -9,18 +9,8 @@ interface Props {
 const props = defineProps<Props>()
 
 const emit = defineEmits<{
-  (e: 'edit', card: Card): void
-  (e: 'delete', cardId: string): void
   (e: 'open', card: Card): void
 }>()
-
-const editCard = () => {
-  emit('edit', props.card)
-}
-
-const deleteCard = () => {
-  emit('delete', props.card.id)
-}
 
 const openCard = () => {
   emit('open', props.card)
@@ -33,14 +23,6 @@ const openCard = () => {
       <div>
         <h3 class="font-medium text-gray-800">{{ card.title }}</h3>
         <p v-if="card.description" class="text-sm text-gray-600 mt-1">{{ card.description }}</p>
-      </div>
-      <div class="flex space-x-2">
-        <button @click.stop="editCard" class="text-blue-500 hover:text-blue-700 transition-colors duration-200">
-          <UIcon name="i-lucide-edit" class="w-4 h-4" />
-        </button>
-        <button @click.stop="deleteCard" class="text-red-500 hover:text-red-700 transition-colors duration-200">
-          <UIcon name="i-lucide-trash-2" class="w-4 h-4" />
-        </button>
       </div>
     </div>
   </div>
