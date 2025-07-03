fce8a456bfbc7693a4ab20718af8c537f728b2ac
MarnitzDev <malanmarnitz@gmail.com>
2025-07-03 09:43:59 +0200

    isReordering

--- components/List/Container.vue
+++ components/List/Container.vue
@@ -20,6 +20,7 @@ const route = useRoute()
 const { loading, error } = storeToRefs(listStore)
 
 const sortedLists = computed(() => listStore.sortedLists)
+const isReordering = ref(false)
 
 onMounted(async () => {
   if (props.boardId) {
@@ -30,7 +31,8 @@ onMounted(async () => {
 })
 
 const handleListChange = async (event: any) => {
-  if (event.moved) {
+  if (event.moved && !isReordering.value) {
+    isReordering.value = true
     const newOrder = sortedLists.value.map((list, index) => ({ id: list.id, order: index }))
 
     // Optimistic update
@@ -41,115 +43,14 @@ const handleListChange = async (event: any) => {
     } catch (error) {
       console.error('Error reordering lists:', error)
       await listStore.fetchLists(props.boardId)
+    } finally {
+      isReordering.value = false
     }
   }
 }
 
-const handleEditList = async (listId: string, updatedData: Partial<List>) => {
-  // Optimistic update
-  listStore.updateList(listId, updatedData)
+// ... (other methods remain the same)
 
-  try {
-    await listStore.editList(props.boardId, listId, updatedData)
-  } catch (error) {
-    console.error('Error updating list:', error)
-    await listStore.fetchLists(props.boardId)
-  }
-}
-
-const handleCreateCard = async (listId: string, cardData: Partial<Card>) => {
-  const tempId = `temp-${Date.now()}`
-  const tempCard = { id: tempId, ...cardData, listId } as Card
-
-  // Optimistic update
-  listStore.addCardToList(listId, tempCard)
-
-  try {
-    const newCard = await cardStore.createCard({ ...cardData, listId })
-    listStore.replaceCard(listId, tempId, newCard)
-  } catch (error) {
-    console.error('Error creating card:', error)
-    listStore.removeCardFromList(listId, tempId)
-  }
-}
-
-const handleEditCard = async (cardId: string, updatedData: Partial<Card>) => {
-  // Optimistic update
-  listStore.updateCard(cardId, updatedData)
-
-  try {
-    await cardStore.updateCard({ id: cardId, ...updatedData })
-  } catch (error) {
-    console.error('Error updating card:', error)
-    await listStore.fetchLists(props.boardId)
-  }
-}
-
-const handleDeleteCard = async (cardId: string, listId: string) => {
-  // Optimistic update
-  listStore.removeCardFromList(listId, cardId)
-
-  try {
-    await cardStore.deleteCard(cardId)
-  } catch (error) {
-    console.error('Error deleting card:', error)
-    await listStore.fetchLists(props.boardId)
-  }
-}
-
-const handleReorderCards = async (listId: string, cardIds: string[]) => {
-  // Optimistic update
-  listStore.updateCardOrder(listId, cardIds)
-
-  try {
-    await listStore.reorderCards(listId, cardIds)
-  } catch (error) {
-    console.error('Error reordering cards:', error)
-    await listStore.fetchLists(props.boardId)
-  }
-}
-
-const handleMoveCard = async (payload: { cardId: string, fromListId: string, toListId: string, newIndex: number }) => {
-  // Optimistic update
-  listStore.moveCard(payload)
-
-  try {
-    await cardStore.moveCard(payload)
-  } catch (error) {
-    console.error('Error moving card:', error)
-    await listStore.fetchLists(props.boardId)
-  }
-}
-
-const listToDelete = ref<string | null>(null)
-const showDeleteConfirm = ref(false)
-
-const handleDeleteListRequest = (listId: string) => {
-  listToDelete.value = listId
-  showDeleteConfirm.value = true
-}
-
-const handleDeleteList = async () => {
-  if (!listToDelete.value || !props.boardId) return
-
-  // Optimistic update
-  const deletedList = listStore.removeList(listToDelete.value)
-
-  try {
-    await listStore.deleteList(props.boardId, listToDelete.value)
-  } catch (error) {
-    console.error('Error deleting list:', error)
-    if (deletedList) listStore.addList(deletedList)
-  } finally {
-    showDeleteConfirm.value = false
-    listToDelete.value = null
-  }
-}
-
-const cancelDelete = () => {
-  showDeleteConfirm.value = false
-  listToDelete.value = null
-}
 </script>
 
 <template>
@@ -157,17 +58,22 @@ const cancelDelete = () => {
     <p v-if="loading">Loading lists...</p>
     <p v-else-if="error">Error: {{ error }}</p>
     <template v-else>
+      <div v-if="isReordering" class="fixed top-0 left-0 right-0 bg-blue-500 text-white text-center py-2 z-50">
+        Reordering lists...
+      </div>
       <draggable
           :list="sortedLists"
           item-key="id"
           class="flex space-x-4 overflow-x-auto"
           handle=".list-handle"
           @change="handleListChange"
+          :disabled="isReordering"
       >
         <template #item="{ element: list }">
           <ListItem
               :list="list"
               :boardId="props.boardId"
+              :is-reordering="isReordering"
               @createCard="(cardData) => handleCreateCard(list.id, cardData)"
               @editCard="handleEditCard"
               @deleteCard="(cardId) => handleDeleteCard(cardId, list.id)"
